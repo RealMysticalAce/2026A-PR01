7945b007627aef7c51952f5c983ffca4f86c4a17
RealMysticalAce <valeriyyauseichyk@gmail.com>
2026-09-21 12:36:58 -0400

    rerjouter code efface partie 2.2

--- platforms.py
+++ platforms.py
@@ -63,14 +63,15 @@ def choose_platform_type(green_probability, blue_probability, spring_probability
     plateforme marron.
     """
 
-    # TODO : Utilisez random.random() et les probabilités reçues en paramètres
-    # pour retourner l'une des chaînes suivantes :
-    # "green", "blue", "spring" ou "brown".
-    #
-    # Attention : les seuils utilisés avec random.random() doivent être
-    # cumulatifs.
-
-    return "green"  # Valeur temporaire à remplacer
+    r = random.random()
+    if r < green_probability:
+        return "green"
+    elif r < green_probability + blue_probability:
+        return "blue"
+    elif r < green_probability + blue_probability + spring_probability:
+        return "spring"
+    else:
+        return "brown"
 
 # ===========================================================
 
